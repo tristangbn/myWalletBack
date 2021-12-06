da01d78d0701c6f6b6d721c24b6f955a4f1956b6
François Cordier <francoiscordier.ece@gmail.com>
2021-12-06 11:08:03 +0100

    Sign-in route

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,11 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
+// Inscription d'un utilisateur
 router.post("/sign-up", async function (req, res) {
   const { firstName, lastName, email, password } = req.body;
 
+  // Hash du mot de passe en DB et ajout de l'utilisateur
   bcrypt.hash(password, saltRounds, async function (err, hash) {
     const newUser = new userModel({
       firstName: firstName,
@@ -27,6 +29,20 @@ router.post("/sign-up", async function (req, res) {
     await newUser.save();
 
     res.json({ result: true, message: "User added to DB" });
+  });
+});
+
+// Connexion d'un utilisateur
+router.post("/sign-in", async function (req, res) {
+  const { email, password } = req.body;
+
+  // Récupération des informations de l'utilisateur en DB
+  const user = await userModel.findOne({ email: email.toLowerCase() });
+
+  // Comparaison du mot de passe entré par l'utilisateur avec celui enregistré en DB
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (result) res.json({ result: true, message: "User connected" });
+    else res.json({ result: false, message: "Wrong credentials" });
   });
 });
 
