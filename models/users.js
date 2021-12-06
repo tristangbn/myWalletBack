a2bfbb2d2900a12e76a791b53f75c60b7289108c
François Cordier <francoiscordier.ece@gmail.com>
2021-12-06 10:56:07 +0100

    Sign-up route

---
+++ models/users.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  inscriptionDate: Date,
+  token: String,
+  ownedCryptos: [
+    {
+      id: String,
+      image: String,
+      name: String,
+      totalQuantity: Number,
+      transactions_id: [
+        { type: mongoose.Schema.Types.ObjectId, ref: "transactions" },
+      ],
+    },
+  ],
+});
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userModel = require("../models/users");
+const transactionModel = require("../models/transactions");
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
+const saltRounds = 10;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/sign-up", async function (req, res) {
+  const { firstName, lastName, email, password } = req.body;
+
+  bcrypt.hash(password, saltRounds, async function (err, hash) {
+    const newUser = new userModel({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hash,
+      inscriptionDate: new Date(),
+      token: uid2(32),
+    });
+
+    await newUser.save();
+
+    res.json({ result: true, message: "User added to DB" });
+  });
 });
 
 module.exports = router;
