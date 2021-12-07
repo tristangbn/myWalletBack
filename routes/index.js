45683697b7d4d13b6bbbafb30c6182070606b5b0
François Cordier <francoiscordier.ece@gmail.com>
2021-12-07 15:47:53 +0100

    Blindage express-validator

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcrypt");
 const uid2 = require("uid2");
 const saltRounds = 10;
 const axios = require("axios");
+const { body, validationResult, check } = require("express-validator");
 
 const coinGeckoAPI = axios.create({
   baseURL: "https://api.coingecko.com/api/v3",
@@ -13,50 +14,135 @@ const coinGeckoAPI = axios.create({
 });
 
 // Inscription d'un utilisateur
-router.post("/sign-up", async function (req, res) {
-  const { firstName, lastName, email, password } = req.body;
+router.post(
+  "/sign-up",
+  body("firstName")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Please enter your first name"),
+  body("lastName")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Please enter your last name"),
+  body("email").custom(async (email) => {
+    return await userModel
+      .findOne({ email: email.toLowerCase() })
+      .then((user) => {
+        if (user) {
+          return Promise.reject("E-mail already in use");
+        }
+      });
+  }),
+  check("password", "The password must be 5+ chars long and contain a number")
+    .not()
+    .isIn(["123", "password", "god", "azerty", "qwerty"])
+    .withMessage("Do not use a common word as the password")
+    .isLength({ min: 5 })
+    .matches(/\d/),
+  check("email").isEmail().withMessage("Please enter a valid email address"),
+  async function (req, res) {
+    const errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+      res.json(errors);
+    } else {
+      const { firstName, lastName, email, password } = req.body;
 
-  // Hash du mot de passe en DB et ajout de l'utilisateur en DB
-  bcrypt.hash(password, saltRounds, async function (err, hash) {
-    const newUser = new userModel({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      password: hash,
-      inscriptionDate: new Date(),
-      token: uid2(32),
-    });
+      // Hash du mot de passe en DB et ajout de l'utilisateur en DB
+      bcrypt.hash(password, saltRounds, async function (err, hash) {
+        const newUser = new userModel({
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: hash,
+          inscriptionDate: new Date(),
+          token: uid2(32),
+        });
 
-    await newUser.save();
+        await newUser.save();
 
-    res.json({
-      result: true,
-      message: "User added to DB",
-      userToken: newUser.token,
-      firstName: newUser.firstName,
-    });
-  });
-});
+        res.json({
+          result: true,
+          message: "User added to DB",
+          userToken: newUser.token,
+          firstName: newUser.firstName,
+        });
+      });
+    }
+  }
+);
 
 // Connexion d'un utilisateur
-router.post("/sign-in", async function (req, res) {
-  const { email, password } = req.body;
-
-  // Récupération des informations de l'utilisateur en DB
-  const user = await userModel.findOne({ email: email.toLowerCase() });
-
-  // Comparaison du mot de passe entré par l'utilisateur avec celui enregistré en DB
-  bcrypt.compare(password, user.password, function (err, result) {
-    if (result)
-      res.json({
-        result: true,
-        message: "User connected",
-        userToken: user.token,
-        firstName: user.firstName,
+router.post(
+  "/sign-in",
+  body("email")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Please enter your e-mail"),
+  body("password")
+    .not()
+    .isEmpty()
+    .trim()
+    .escape()
+    .withMessage("Please enter your password"),
+  check("email").isEmail().withMessage("Please enter a valid email address"),
+  body("email").custom(async (email) => {
+    return await userModel
+      .findOne({ email: email.toLowerCase() })
+      .then((user) => {
+        if (!user) {
+          return Promise.reject("User doesn't exists");
+        }
       });
-    else res.json({ result: false, message: "Wrong credentials" });
-  });
-});
+  }),
+  async function (req, res) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors);
+      res.json(errors);
+    } else {
+      const { email, password } = req.body;
+
+      // Récupération des informations de l'utilisateur en DB
+      const user = await userModel.findOne({ email: email.toLowerCase() });
+
+      // Comparaison du mot de passe entré par l'utilisateur avec celui enregistré en DB
+      bcrypt.compare(password, user.password, function (err, result) {
+        if (result)
+          res.json({
+            result: true,
+            message: "User connected",
+            userToken: user.token,
+            firstName: user.firstName,
+          });
+        else
+          res.json({
+            result: false,
+            errors: [
+              {
+                value: "",
+                msg: "Password or email is incorrect",
+                param: "password",
+                location: "body",
+              },
+              {
+                value: "",
+                msg: "",
+                param: "email",
+                location: "body",
+              },
+            ],
+          });
+      });
+    }
+  }
+);
 
 // Afficher la liste des crypto de l'utilisateur
 router.get("/list-crypto/:token", async function (req, res) {
