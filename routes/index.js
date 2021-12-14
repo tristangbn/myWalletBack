f2adb5dc28f3eeaf9e7b2890234663336dbf2f3d
François Cordier <francoiscordier.ece@gmail.com>
2021-12-14 12:43:04 +0100

    Calcul total portfolio investissement

--- routes/index.js
+++ routes/index.js
@@ -213,7 +213,7 @@ router.get("/list-crypto/:token", async function (req, res) {
               symbol: ownedCryptos[i].symbol,
               totalQuantity: ownedCryptos[i].totalQuantity,
               transactions_id: ownedCryptos[i].transactions_id,
-              current_price: response.data[ownedCryptos[i].id]["eur"],
+              currentPrice: response.data[ownedCryptos[i].id]["eur"],
               _id: ownedCryptos[i]._id,
               totalInvestment: totalInvestmentPerCrypto.find(
                 (el) => el.crypto === ownedCryptos[i].id
@@ -226,12 +226,20 @@ router.get("/list-crypto/:token", async function (req, res) {
             ownedCryptosCopy.push(crypto);
           }
 
-          console.log(ownedCryptosCopy);
+          const totalPortfolioInvestment = totalInvestmentPerCrypto.reduce(
+            (acc, val) => {
+              return acc + val.totalInvestment;
+            },
+            0
+          );
+
+          console.log(totalPortfolioInvestment);
 
           res.json({
             result: true,
             message: "ownedCryptos array correctly loaded",
             ownedCryptos: ownedCryptosCopy,
+            totalPortfolioInvestment,
           });
         });
     } else {
