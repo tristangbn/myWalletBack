ee93ad02fde202a6e5c1afba14909dce3ed5e423
François Cordier <francoiscordier.ece@gmail.com>
2021-12-16 11:13:44 +0100

    Routes refactoring

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var cryptosRouter = require("./routes/cryptos");
+var transactionsRouter = require("./routes/transactions");
 
 var app = express();
 require("./models/db");
@@ -22,6 +24,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/cryptos", cryptosRouter);
+app.use("/transactions", transactionsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
