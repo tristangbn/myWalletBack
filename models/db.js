0124f83946dfe651482b0ab35a427596c342548c
Tristan de Guillebon <tdeguill@gmail.com>
2022-06-25 17:16:06 +0200

    Removed mongo codes

--- models/db.js
+++ models/db.js
@@ -7,8 +7,7 @@ const options = {
 };
 
 mongoose.connect(
-  "mongodb+srv://admin:admin@cluster0.m8yb3.mongodb.net/myWallet",
-  // "mongodb+srv://admin:200392@cluster0.cpene.mongodb.net/myWallet?retryWrites=true&w=majority",
+ 
   options,
   function (err) {
     console.log(err || "Connected to MongoDB");
